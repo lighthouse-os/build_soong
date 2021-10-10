f266e9a69a006e05a27c858805349353b690d77c
Stealth1226 <rishawniyer06@gmail.com>
2021-10-10 07:43:50 +0000

    Revert "Allow kernel build tools"
    This reverts commit 1f853810eda23ef4c942b42109b1d36c2adcee0e.

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -196,8 +196,6 @@ var Configuration = map[string]PathConfig{
 	"arm-linux-androidkernel-strings":    Allowed,
 	"arm-linux-androidkernel-strip":      Allowed,
 
-	"arm-linux-androidkernel-as": Allowed,
-	"arm-linux-androidkernel-ld": Allowed,
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
 	"ar":         Forbidden,
